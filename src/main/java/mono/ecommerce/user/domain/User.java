5ef741e27e354167de359528eb0b4a4ec3e6d50d
lass <lass9436@gmail.com>
2024-06-02 21:33:19 +0900

    User 엔티티 컬럼 설정 추가 및 생성자 메서드 추가

--- src/main/java/mono/ecommerce/user/domain/User.java
+++ src/main/java/mono/ecommerce/user/domain/User.java
@@ -1,18 +1,31 @@
 package mono.ecommerce.user.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import mono.ecommerce.security.SignRequest;
 
 @Entity
 @Table(name = "users")
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true, nullable = false)
     private String username;
+    @Column(nullable = false)
     private String password;
+    @Column(nullable = false)
     private String nickname;
     private String role;
 
+    public User(SignRequest signRequest) {
+        this.username = signRequest.getUsername();
+        this.password = signRequest.getPassword();
+        this.nickname = signRequest.getNickname();
+        this.role = "USER";
+    }
 }
