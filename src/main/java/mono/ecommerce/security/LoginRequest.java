9428768ecb7f757ac6879f5805537e4863a46e86
lass <lass9436@gmail.com>
2024-06-02 17:16:25 +0900

    LoginRequest 작성

---
+++ src/main/java/mono/ecommerce/security/LoginRequest.java
@@ -0,0 +1,19 @@
+package mono.ecommerce.security;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequest {
+    private String username;
+    private String password;
+
+    @Override
+    public String toString() {
+        return "LoginRequest{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
