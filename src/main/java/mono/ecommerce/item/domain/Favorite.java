80d25d3cf63810749adb7cf34d5bcc7b7d990c00
lass <lass9436@gmail.com>
2024-06-03 02:17:32 +0900

    Favorite (즐겨찾기) 엔티티 추가

---
+++ src/main/java/mono/ecommerce/item/domain/Favorite.java
@@ -0,0 +1,25 @@
+package mono.ecommerce.item.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import mono.ecommerce.user.domain.User;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Favorite {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id", nullable = false)
+    private Item item;
+}
