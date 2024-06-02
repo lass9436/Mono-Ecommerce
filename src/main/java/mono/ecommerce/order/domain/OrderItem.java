1bc685192d5da500f54364183c4d6b985fbccdf3
lass <lass9436@gmail.com>
2024-06-03 01:57:47 +0900

    OrderItem 엔티티 추가

---
+++ src/main/java/mono/ecommerce/order/domain/OrderItem.java
@@ -0,0 +1,31 @@
+package mono.ecommerce.order.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import mono.ecommerce.item.domain.Item;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id", nullable = false)
+    private Item item;
+
+    @Column(nullable = false)
+    private Long quantity;
+
+    @Column(nullable = false)
+    private Long totalPrice;
+}
