026e3ceaf68bbf97e53405a907657f36be36030b
lass <lass9436@gmail.com>
2024-06-02 21:32:43 +0900

    h2-console 보기 위해 SecurityConfig 수정

--- src/main/java/mono/ecommerce/security/SecurityConfig.java
+++ src/main/java/mono/ecommerce/security/SecurityConfig.java
@@ -4,9 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 
 @Configuration
 public class SecurityConfig{
@@ -22,16 +24,20 @@ public class SecurityConfig{
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .formLogin(AbstractHttpConfigurer::disable)
-                .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(request -> {
-                    request
-                            .requestMatchers("/login", "/").permitAll()
-                            .requestMatchers("/user/**").hasRole("USER")
-                            .anyRequest().permitAll();
-                })
-                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider, userDetailsService), UsernamePasswordAuthenticationFilter.class);
+            .headers(headers -> {
+                headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin);
+            })
+            .formLogin(AbstractHttpConfigurer::disable)
+            .csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(request -> {
+                request
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/user/**").hasRole("USER")
+                        .anyRequest().permitAll();
+            })
+            .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider, userDetailsService), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
