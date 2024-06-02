69d94945ef5cd72987039c7c919c4339b5cf2c39
lass <lass9436@gmail.com>
2024-06-02 22:48:24 +0900

    schema, data 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO users (username, password, nickname, role)
-VALUES ('username1', '$2a$10$..k52ypcmQn/bFasGMmiWe3Cvi/Hk.Tl7C.4Adi72/HT3jkQYOfR6', 'nickname1', 'USER'),
-       ('username2', '$2a$10$o3TkcPkjyzAzOnNAFCnGc.zuluHL91zX9ekE4ZWuKQmQirp2iR31q', 'nickname2', 'USER'),
-       ('username3', '$2a$10$aYr10cgx6sQAmGj4GGdo.OISDYc2g7EqU8FtdmMJKDQlqd1.ZFCPO', 'nickname3', 'USER');
+INSERT INTO users (username, password, nickname, role, point)
+VALUES ('username1', '$2a$10$..k52ypcmQn/bFasGMmiWe3Cvi/Hk.Tl7C.4Adi72/HT3jkQYOfR6', 'nickname1', 'USER', 100000),
+       ('username2', '$2a$10$o3TkcPkjyzAzOnNAFCnGc.zuluHL91zX9ekE4ZWuKQmQirp2iR31q', 'nickname2', 'USER', 50000),
+       ('username3', '$2a$10$aYr10cgx6sQAmGj4GGdo.OISDYc2g7EqU8FtdmMJKDQlqd1.ZFCPO', 'nickname3', 'USER', 25000);
