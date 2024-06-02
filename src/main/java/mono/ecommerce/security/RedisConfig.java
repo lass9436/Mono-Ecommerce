672bdc48c1a917d25537c67838b6c09e68b34c72
lass <lass9436@gmail.com>
2024-06-02 22:48:35 +0900

    Redis 설정 추가

---
+++ src/main/java/mono/ecommerce/security/RedisConfig.java
@@ -0,0 +1,17 @@
+package mono.ecommerce.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+}
