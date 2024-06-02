ab276b452a6cafc4df480d7f7526908577e52e3c
lass <lass9436@gmail.com>
2024-06-03 02:10:28 +0900

    Item 전체 조회 기능 추가

---
+++ src/main/java/mono/ecommerce/item/controller/ItemController.java
@@ -0,0 +1,24 @@
+package mono.ecommerce.item.controller;
+
+import mono.ecommerce.item.service.ItemService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/item")
+public class ItemController {
+
+    private final ItemService itemService;
+
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @GetMapping
+    public List<ItemDto> getItems() {
+        return itemService.findAll();
+    }
+}
