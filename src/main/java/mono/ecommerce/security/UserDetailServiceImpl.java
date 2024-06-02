a482d823339fb473df8c4da10e40a587d5acaa4a
lass <lass9436@gmail.com>
2024-06-03 02:36:54 +0900

    UserDetailServiceImpl 수정

--- src/main/java/mono/ecommerce/security/UserDetailServiceImpl.java
+++ src/main/java/mono/ecommerce/security/UserDetailServiceImpl.java
@@ -26,7 +26,7 @@ public class UserDetailServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         final User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("user not found"));
-        return new UserDetailsImpl(user.getUsername(), user.getPassword(), List.of(user.getRole()));
+        return new UserDetailsImpl(user.getId(), user.getUsername(), user.getPassword(), List.of(user.getRole()));
     }
 
     public User findByUsernameAndPassword(String username, String password) throws UsernameNotFoundException {
