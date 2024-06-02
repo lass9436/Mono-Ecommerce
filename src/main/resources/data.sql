d94d717c72c08dc32feee8a4e396ee804795d40f
lass <lass9436@gmail.com>
2024-06-02 17:13:53 +0900

    User 초기 스키마, 초기 데이터 작성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (username, password, role, nickname) VALUES
+   ('username1', 'password1', 'USER', 'nickname1'),
+   ('username2', 'password2', 'USER', 'nickname2'),
+   ('username3', 'password3', 'ADMIN', 'nickname3');
