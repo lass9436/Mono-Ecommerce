5142fb741da2d3a8ff17d5650bdcceb6c326ccc9
lass <lass9436@gmail.com>
2024-06-03 01:57:32 +0900

    Item 엔티티 추가

---
+++ src/main/java/mono/ecommerce/item/domain/Item.java
@@ -0,0 +1,25 @@
+package mono.ecommerce.item.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Long price;
+
+    @Column(nullable = false)
+    private Long quantity;
+}
