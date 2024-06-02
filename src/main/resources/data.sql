49d52c8cca19f6c555ecfcc67c10a2676a58b6d7
lass <lass9436@gmail.com>
2024-06-02 21:45:58 +0900

    암호화된 샘플 데이터로 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO users (username, password, role, nickname) VALUES
-   ('username1', 'password1', 'USER', 'nickname1'),
-   ('username2', 'password2', 'USER', 'nickname2'),
-   ('username3', 'password3', 'ADMIN', 'nickname3');
+INSERT INTO users (username, password, nickname, role)
+VALUES ('username1', '$2a$10$..k52ypcmQn/bFasGMmiWe3Cvi/Hk.Tl7C.4Adi72/HT3jkQYOfR6', 'nickname1', 'USER'),
+       ('username2', '$2a$10$o3TkcPkjyzAzOnNAFCnGc.zuluHL91zX9ekE4ZWuKQmQirp2iR31q', 'nickname2', 'USER'),
+       ('username3', '$2a$10$aYr10cgx6sQAmGj4GGdo.OISDYc2g7EqU8FtdmMJKDQlqd1.ZFCPO', 'nickname3', 'USER');
